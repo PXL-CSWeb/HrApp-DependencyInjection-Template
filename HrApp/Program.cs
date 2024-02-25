2dccec1287040ec316f20a518c9796437f07816e
Wim Bervoets <wim.bervoets@pxl.be>
2024-02-25 19:02:02 +0100

    remove LoginViewModel

--- HrApp/Program.cs
+++ HrApp/Program.cs
@@ -37,6 +37,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.EnsureDbMigrated();
+EnsureDbMigrated(app);
 
 app.Run();
--- HrApp/ViewModels/LoginEmailViewModel.cs
+++ HrApp/ViewModels/LoginEmailViewModel.cs
@@ -5,6 +5,7 @@ namespace HrApp.ViewModels
     public class LoginEmailViewModel
     {
         [EmailAddress]
+        [Required]
         public string Email { get; set; } = string.Empty;
         [DataType(DataType.Password)]
         public string Password { get; set; } = string.Empty;
